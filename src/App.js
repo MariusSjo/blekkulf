4f93879e123fe821b51a0f77a8eea405c9a7dbb4
Marius Sjøberg <mariuscs@stud.ntnu.no>
2021-10-19 13:10:34 +0200

    last commit

--- src/App.js
+++ src/App.js
@@ -6,32 +6,30 @@ import Cover from "./cover.js";
 import "./App.css";
 import YouTube from "react-youtube";
 
-let teams = ["turquoise", "yellow", "pink"];
+let teams = ["turquoise", "pink"];
 
 let persons = [
-  { team: "yellow", name: "Torkjell", competition: "Ddakji", poeng: "5/-5" },
-  { team: "blue", name: "Marius", competition: "Singstar", poeng: "10" },
+  { team: "blue", name: "Øyvind", competition: "Breezerrace", poeng: "7" },
+  { team: "blue", name: "Anniken", competition: "Veddeløp", poeng: "-10" },
   { team: "pink", name: "Hanne", competition: "Opus", poeng: "-10" },
-  { team: "yellow", name: "Anders", competition: "Confidence", poeng: "-2" },
+  { team: "pink", name: "Gina", competition: "Ddakji", poeng: "5/-5" },
+  { team: "blue", name: "Anders", competition: "Confidence", poeng: "-2" },
   { team: "pink", name: "Julia", competition: "Vodka eller vann", poeng: "2" },
-  { team: "pink", name: "Anniken", competition: "Ring of fire", poeng: "-10" },
-  { team: "blue", name: "Hedvig", competition: "Veddeløp", poeng: "7" },
-  { team: "yellow", name: "Øyvind", competition: "Breezerrace", poeng: "7" },
-  {
-    team: "yellow",
-    name: "Sara",
-    competition: "Glassbro/bussrute",
-    poeng: "5/-3",
-  },
   { team: "blue", name: "Sander", competition: "Sukkerkjeks", poeng: "10/-5" },
-  { team: "blue", name: "Thomas", competition: "Terningspill", poeng: "5" },
-  { team: "yellow", name: "Gina", competition: "Baby freeze", poeng: "10" },
   {
     team: "pink",
+    name: "Sara",
+    competition: "Glassbro",
+    poeng: "2/-3",
+  },
+  { team: "pink", name: "Thomas", competition: "Kulespill", poeng: "5" },
+  {
+    team: "blue",
     name: "MrBursdag",
     competition: "Flip the cup",
     poeng: "10",
   },
+  { team: "blue", name: "Marius", competition: "Singstar", poeng: "10" },
 ];
 
 const opts = {
@@ -43,44 +41,68 @@ const opts = {
 };
 
 function App() {
-  const [hideGame, setHideGame] = useState("")
-  const [hideGame2, setHideGame2] = useState("")
+  const [hideGame, setHideGame] = useState("");
+  const [hideGame2, setHideGame2] = useState("");
   const [reveal, setreveal] = useState(0);
   return (
     <div className="App">
-          <div onClick={()=>{setHideGame("hidden")}} className={`cover ${hideGame}`}>
-          <div className="coverContent">
-            <h1>Regler</h1>
-            <p>1. Antall plusspoeng er antall slurker man kan gi ut.(Gjelder lagvis)</p>
-            <p>2. Antall minuspoeng er antall slurker man skal drikke.</p>
-            <p>3. Alle har en konkurranse dedikert til seg man ikke kan delta på.</p>
-            <p>4. Ha det gøy.</p>
-            <p><b>Nå er det tid for å komme seg inn i tracksuitene!</b></p>
+      <div
+        onClick={() => {
+          setHideGame("hidden");
+        }}
+        className={`cover ${hideGame}`}
+      >
+        <div className="coverContent">
+          <h1>Regler</h1>
+          <p>
+            1. Antall plusspoeng er antall slurker man kan gi ut.(Gjelder
+            lagvis)
+          </p>
+          <p>2. Antall minuspoeng er antall slurker man skal drikke.</p>
+          <p>
+            3. Alle har en konkurranse dedikert til seg man ikke kan delta på.
+          </p>
+          <p>4. Ha det gøy.</p>
+          <p>
+            <b>Nå er det tid for å komme seg inn i tracksuitene!</b>
+          </p>
+        </div>
+      </div>
+      <div
+        onClick={() => {
+          setHideGame2("hidden");
+        }}
+        className={`cover ${hideGame2}`}
+      >
+        <div className="coverContent">
+          <Cover />
+        </div>
+      </div>
 
-            </div>
-          </div>
-          <div onClick={()=>{setHideGame2("hidden")}} className={`cover ${hideGame2}`}> 
-          <div className="coverContent">
-            <Cover/>
-            </div>
-          </div>
-          
-          <div className="points">
-          {teams.map((team) => (
-            <Points team={team} />
-          ))}
-          </div>
-          <Countdown/>
+      <div className="points">
+        {teams.map((team) => (
+          <Points team={team} />
+        ))}
+      </div>
+      <Countdown />
       <header className="App-header">
         <img id="poengbakgrunn" src="img/poengbakgrunn.png" alt="scetchy"></img>
       </header>
-      {!reveal && <button className="button" onClick={() => setreveal(1)}>Avslør lagene!</button>}
+      {!reveal && (
+        <button className="button" onClick={() => setreveal(1)}>
+          Avslør lagene!
+        </button>
+      )}
       <div className="cardContainer">
         {persons.map((person) => (
           <Card key={person.name} person={person} reveal={reveal} />
-        ))}    
+        ))}
       </div>
-      {hideGame2==="hidden" && <div style={{position: "fixed"}}><YouTube classname="youtube" videoId="v9NQYKv2rTg" opts={opts} /></div>}
+      {hideGame2 === "hidden" && (
+        <div style={{ position: "fixed" }}>
+          <YouTube classname="youtube" videoId="v9NQYKv2rTg" opts={opts} />
+        </div>
+      )}
     </div>
   );
 }
